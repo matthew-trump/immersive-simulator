6d385d9552d0c5ec927fd2829d2035668ea4158c
matthew-trump <matthew.trump@gmail.com>
2019-04-18 10:09:58 -0700

    interface for requests to dialogflow

---
+++ src/app/dialogflow-request.ts
@@ -0,0 +1,102 @@
+import { DialogflowSession } from "./dialogflow-session";
+
+const INPUT_TYPE_KEYBOARD: String = "KEYBOARD";
+const INPUT_TYPE_VOICE: String = "VOICE";
+
+export class DialogflowRequest {
+
+    constructor(
+        public projectId: string,
+        public session: DialogflowSession,
+        public conversationType: string,
+        public intent: string,
+        public inputType: string,
+        public query: string) { }
+
+    getObject(): any {
+        const obj: any = {
+            user: {
+                userId: this.session.user.id,
+                locale: this.session.user.locale,
+                lastSeen: this.session.user.lastSeen,
+                userStorage: this.session.user.userStorage
+            },
+            conversation: {
+                conversationId: this.session.conversationId,
+                conversationToken: this.session.conversationToken,
+                type: this.conversationType //ACTIVE or NEW
+            },
+            inputs: [
+                {
+                    intent: this.intent,
+                    rawInputs: [
+                        {
+                            inputType: this.inputType,
+                            query: this.query
+                        }
+                    ],
+                    arguments: [
+                        {
+                            "name": "text",
+                            "rawText": this.query,
+                            "textValue": this.query
+                        }
+                    ]
+                }
+            ],
+            surface: {
+                capabilities: [
+                    {
+                        name: "actions.capability.SCREEN_OUTPUT"
+                    },
+                    {
+                        name: "actions.capability.MEDIA_RESPONSE_AUDIO"
+                    },
+                    {
+                        name: "actions.capability.WEB_BROWSER"
+                    },
+                    {
+                        name: "actions.capability.AUDIO_OUTPUT"
+                    }
+                ]
+            },
+            isInSandbox: true,
+            availableSurfaces: [
+                {
+                    capabilities: [
+                        {
+                            name: "actions.capability.SCREEN_OUTPUT"
+                        },
+                        {
+                            name: "actions.capability.WEB_BROWSER"
+                        },
+                        {
+                            name: "actions.capability.AUDIO_OUTPUT"
+                        }
+                    ]
+                }
+            ],
+            requestType: "SIMULATOR"
+        }
+
+        if (this.session.user.useCanvas) {
+            obj.surface.capabilities.push({
+                name: "actions.capability.CUSTOM_STAGE"
+            });
+        }
+
+        if (obj.inputs[0].rawInputs[0].inputType === INPUT_TYPE_KEYBOARD) {
+            obj.inputs[0].arguments = [
+                {
+                    "name": "text",
+                    "rawText": this.query,
+                    "textValue": this.query
+                }
+            ]
+        } else if (obj.inputs[0].rawInputs[0].inputType === INPUT_TYPE_VOICE) {
+
+        }
+        return obj;
+    }
+
+}
