509f2a588d3e8e254d96ea8ce953f6f5a5101429
matthew-trump <matthew.trump@gmail.com>
2019-04-18 15:42:33 -0700

    constants used in app

---
+++ src/app/google-assistant.ts
@@ -0,0 +1,19 @@
+export enum CONVERSATION_TYPE {
+    NEW = "NEW",
+    ACTIVE = "ACTIVE",
+}
+export enum ACTIONS_INTENT {
+    MAIN = "actions.intent.MAIN",
+    TEXT = "actions.intent.TEXT"
+}
+export enum INPUT_TYPE {
+    KEYBOARD = "KEYBOARD",
+    VOICE = "VOICE"
+}
+export enum ACTIONS_CAPABILITY {
+    SCREEN_OUTPUT = "actions.capability.SCREEN_OUTPUT",
+    MEDIA_RESPONSE_AUDIO = "actions.capability.MEDIA_RESPONSE_AUDIO",
+    WEB_BROWSER = "actions.capability.WEB_BROWSER",
+    AUDIO_OUTPUT = "actions.capability.AUDIO_OUTPUT",
+    CUSTOM_STAGE = "actions.capability.CUSTOM_STAGE"
+}
