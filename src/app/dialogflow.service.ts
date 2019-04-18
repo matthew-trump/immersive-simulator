a718523e2e04a18bb3a30763ae8a9b25ba852f1b
matthew-trump <matthew.trump@gmail.com>
2019-04-18 15:42:20 -0700

    adjust args to invocation call

--- src/app/dialogflow.service.ts
+++ src/app/dialogflow.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { ErrorMessageService } from './error-message.service';
 import { BackendApiService } from './backend-api.service';
 import { DialogflowRequest } from './dialogflow-request';
+import { DialogflowInvocationOptions } from './dialogflow-invocation-options';
 import { tap, catchError, map } from 'rxjs/operators';
 import { of } from 'rxjs';
 
@@ -10,6 +11,7 @@ import { environment } from '../environments/environment';
 import { DialogflowUserService } from './dialogflow-user.service';
 import { DialogflowSessionService } from './dialogflow-session.service';
 import { CONVERSATION_TYPE, ACTIONS_INTENT, INPUT_TYPE } from './google-assistant';
+import { DialogflowSession } from './dialogflow-session';
 
 @Injectable({
     providedIn: 'root'
@@ -19,16 +21,17 @@ export class DialogflowService {
     constructor(private backendApiService: BackendApiService,
         private dialogflowUserService: DialogflowUserService,
         private dialogflowSessionService: DialogflowSessionService
-    ) {}
+    ) { }
 
-    sendInvocation(projectId: string, text: string, options: any) {
-        return this.backendApiService.postToDialogflow(new DialogflowRequest(
-            projectId,
-            this.dialogflowSessionService.getNewSession(this.dialogflowUserService.getCurrentUser()),
-            CONVERSATION_TYPE.NEW,
-            ACTIONS_INTENT.MAIN,
-            INPUT_TYPE.KEYBOARD,
-            text), options.extra);
+    sendInvocation(projectId: string, text: string, options: DialogflowInvocationOptions) {
+        return this.backendApiService.postToDialogflow(
+            new DialogflowRequest(
+                projectId,
+                this.dialogflowSessionService.getNewSession(this.dialogflowUserService.getCurrentUser(), options.customStage),
+                CONVERSATION_TYPE.NEW,
+                ACTIONS_INTENT.MAIN,
+                INPUT_TYPE.KEYBOARD,
+                text), options.extra);
     }
     sendTextQuery(projectId: string, text: string) {
         return this.backendApiService.postToDialogflow(new DialogflowRequest(
