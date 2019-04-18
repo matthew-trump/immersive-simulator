f60e7ea7cf21dafe3c74cfe7ac83d86cf0c52357
matthew-trump <matthew.trump@gmail.com>
2019-04-18 15:45:09 -0700

    component declarations and module inclusion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,74 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle'
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+
+import { AuthInterceptor } from './auth.interceptor';
+import { AuthGuard } from './auth.guard';
+import { AppRoutingModule } from './app-routing.module';
+import { ConfigStateReducer } from './config-state.reducer';
+import { ProjectReducer } from './project.reducer';
+
+
+
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ConfigComponent } from './config/config.component';
+import { TestApiComponent } from './test-api/test-api.component';
+import { ConversationComponent } from './conversation/conversation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ConfigComponent,
+    TestApiComponent,
+    ConversationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    StoreModule.forRoot({ config: ConfigStateReducer, project: ProjectReducer }),
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatProgressSpinnerModule,
+    MatButtonModule,
+    MatTooltipModule,
+    MatIconModule,
+    MatSelectModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatSlideToggleModule
   ],
-  providers: [],
+  providers: [
+    Title,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    },
+    AuthGuard
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
