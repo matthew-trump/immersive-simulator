ccc16a70bbae7b61155a39fff80c122e1054eef4
matthew-trump <matthew.trump@gmail.com>
2019-04-19 11:50:25 -0700

    expand to include errors

--- src/app/dialogflow-response.ts
+++ src/app/dialogflow-response.ts
@@ -1,8 +1,25 @@
 export class DialogflowResponse {
-    payload: any;
+    body: any;
+    immersive: boolean;
+    immersiveResponse: any;
+    intent: string;
+    error: string;
+
     constructor(data: any) {
-        if (data && data.expectedInputs) {
-            this.payload = data.expectedInputs[0].inputPrompt.richInitialPrompt.items[0];
+        if (data) {
+            if (!data.error) {
+                this.body = data.response;
+                this.intent = this.body.responseMetadata.queryMatchInfo.intent;
+                if (this.body.expectedInputs) {
+                    this.immersive = true;
+                    this.immersiveResponse = this.body.expectedInputs[0].inputPrompt.richInitialPrompt.items[0].immersiveResponse;
+                }
+            } else {
+                this.error = data.error;
+            }
+
+
         }
     }
 }
+
