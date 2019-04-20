2053e914601147dd4bab1ee92540cebecc130532
matthew-trump <matthew.trump@gmail.com>
2019-04-19 20:37:25 -0700

    refactor to accomodate multiple items per response

--- src/app/dialogflow-response.ts
+++ src/app/dialogflow-response.ts
@@ -1,19 +1,54 @@
+export enum ITEM_TYPES {
+    IMMERSIVE = "immersive",
+    SIMPLE = "simple"
+}
+export interface DialogflowResponseItem {
+    type: string;
+}
+
+export class DialogflowImmersiveResponseItem {
+    readonly type: string = ITEM_TYPES.IMMERSIVE;
+    loadImmersiveUrl: string;
+    updatedState: any;
+    constructor(immersiveResponse: any) {
+        this.loadImmersiveUrl = immersiveResponse.loadImmersiveUrl;
+        this.updatedState = immersiveResponse.updatedState;
+    }
+
+}
+export class DialogflowSimpleResponseItem {
+    readonly type: string = ITEM_TYPES.SIMPLE;
+    textToSpeech: string;
+    displayText: string;
+    constructor(simpleResponse: any) {
+        this.textToSpeech = simpleResponse.textToSpeech;
+        this.displayText = simpleResponse.displayText;
+    }
+}
+
 export class DialogflowResponse {
     body: any;
-    immersive: boolean;
-    immersiveResponse: any;
-    intent: string;
     error: string;
+    items: DialogflowResponseItem[];
+    intent: string;
+
 
     constructor(data: any) {
         if (data) {
             if (!data.error) {
                 this.body = data.response;
                 this.intent = this.body.responseMetadata.queryMatchInfo.intent;
-                if (this.body.expectedInputs) {
-                    this.immersive = true;
-                    this.immersiveResponse = this.body.expectedInputs[0].inputPrompt.richInitialPrompt.items[0].immersiveResponse;
-                }
+                this.items = this.body.expectedInputs[0].inputPrompt.richInitialPrompt.items
+                    .map((item: any) => {
+                        if (typeof item.immersiveResponse !== 'undefined') {
+                            return new DialogflowImmersiveResponseItem(item.immersiveResponse)
+                        } else if (typeof item.simpleResponse !== 'undefined') {
+                            return new DialogflowSimpleResponseItem(item.simpleResponse);
+                        } else {
+                            return null;
+                        }
+                    })
+                    .filter((item: any) => item !== null);
             } else {
                 this.error = data.error;
             }
