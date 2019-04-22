1147567c0178974847dcd2d622197292b8f811ea
matthew-trump <matthew.trump@gmail.com>
2019-04-22 09:26:46 -0700

    relay service to get tts audio url from server

---
+++ src/app/text-to-speech.service.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@angular/core';
+import { BackendApiService } from './backend-api.service';
+import { tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TextToSpeechService {
+  public audio: HTMLAudioElement;
+  urlMap: any = {};
+
+  constructor(private backendApiService: BackendApiService) { }
+
+
+  fromChild(requestId: number, tts: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.getAudioUrl(tts).then((result: any) => {
+        if (result.url) {
+          this.audio = new Audio();
+          const audio = this.audio;
+          audio.src = result.url;
+          audio.load();
+          audio.play();
+          audio.onended = () => {
+            resolve(requestId);
+          }
+        }
+      }).catch((err: any) => {
+        reject();
+      })
+    })
+  }
+
+  playAudio(ssml: string) {
+    this.getAudioUrl(ssml).then((result: any) => {
+      console.log("SSML RESULT", result);
+      if (result.url) {
+        this.audio = new Audio();
+        const audio = this.audio;
+        audio.src = result.url;
+        audio.load();
+        audio.play();
+        audio.onended = () => {
+          console.log("AUDIO DONE PLAYING");
+        }
+      }
+    }).catch((err: any) => {
+      console.log("SSML ERROR", err);
+    })
+  }
+  async getAudioUrl(ssml: string): Promise<any> {
+    if (this.urlMap[ssml]) {
+      return Promise.resolve({ url: this.urlMap[ssml], cached: true })
+    }
+    return this.backendApiService.postTextToSpeech(ssml)
+      .pipe(
+        tap((result: any) => {
+          if (result.url) {
+            this.urlMap[ssml] = result.url;
+          }
+        })
+      ).toPromise();
+  }
+
+}
