149cbdd7f7dff5af12f98d2d45cf6dc0a579fb30
matthew-trump <matthew.trump@gmail.com>
2019-04-23 19:33:17 -0700

    implementing methods to contact child service

--- src/app/immersive-simulator.service.ts
+++ src/app/immersive-simulator.service.ts
@@ -1,22 +1,80 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { BehaviorSubject, Subject } from 'rxjs';
+import { tap, takeUntil } from 'rxjs/operators';
 import { ChildService } from './child.service';
+import { DialogflowResponse, DialogflowResponseItem, DialogflowImmersiveResponseItem, ITEM_TYPES } from './dialogflow-response';
+
+const START_URL: string = "/assets/assistant.html";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ImmersiveSimulatorService {
 
-  response$: BehaviorSubject<any> = new BehaviorSubject(null);
-  constructor(private childService: ChildService) {
+  url$: BehaviorSubject<string> = new BehaviorSubject(START_URL);
+  response$: BehaviorSubject<DialogflowResponse> = new BehaviorSubject(null);
+  message$: BehaviorSubject<any>;
+  unsubscribe$: Subject<null> = new Subject();
+
+  constructor(public childService: ChildService) {
+    this.reset();
 
     this.response$
       .pipe(
-        tap((response: any) => {
-          this.childService.handleImmersiveResponse(response);
+        tap((response: DialogflowResponse) => {
+          if (response) {
+            console.log("ImmersiveSimulatorService RESPONSE", response);
+            const items = response.items;
+            items.forEach((item: DialogflowResponseItem) => {
+
+              if (item.type === ITEM_TYPES.IMMERSIVE) {
+                const immersiveResponse: DialogflowImmersiveResponseItem = (item as DialogflowImmersiveResponseItem);
+                const url: string = immersiveResponse.loadImmersiveUrl;
+                if (url) {
+                  this.url$.next(url);
+                }
+                const updatedState: any = immersiveResponse.updatedState;
+                if (this.message$) {
+                  this.message$.next({
+                    type: "payload",
+                    requestId: "" + response.requestId,
+                    data: updatedState
+                  })
+                }
+              }
+            })
+          }
         })
       )
       .subscribe();
+
+  }
+  reset() {
+    this.url$.next(START_URL);
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+    this.message$ = new BehaviorSubject(null);
+    this.unsubscribe$ = new Subject();
+  }
+  loaded() {
+    this.message$
+      .pipe(
+        tap((message: any) => {
+          this.childService.sendMessage(message);
+        }),
+        takeUntil(this.unsubscribe$)
+      )
+      .subscribe()
+  }
+
+  onChildQuery(query: string) {
+
+  }
+  onChildTts(requestId: number, tts: string) {
+
+  }
+
+  handleResponse(response: DialogflowResponse) {
+
   }
 }
