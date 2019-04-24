f7928f38c9f23d6323a5f4bdc3c0af877f03bf61
matthew-trump <matthew.trump@gmail.com>
2019-04-23 19:33:59 -0700

    handling of events in simulator

--- src/app/immersive-simulator/immersive-simulator.component.ts
+++ src/app/immersive-simulator/immersive-simulator.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ImmersiveSimulatorService } from '../immersive-simulator.service';
+import { environment } from '../../environments/environment';
+
+
 
 @Component({
-  selector: 'app-immersive-simulator',
+  selector: 'immersive-simulator',
   templateUrl: './immersive-simulator.component.html',
   styleUrls: ['./immersive-simulator.component.scss']
 })
 export class ImmersiveSimulatorComponent implements OnInit {
 
-  constructor() { }
+  iframe: any = {
+    height: 700,
+    width: 400
+  };
+
+  constructor(public immersiveSimulator: ImmersiveSimulatorService) { }
 
   ngOnInit() {
+  }
+  iframeLoaded() {
+    console.log("IFRAME LOADED");
   }
 
 }
