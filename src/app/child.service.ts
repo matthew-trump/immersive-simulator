9cd5001c34f4f04cf3fadbfa035560fac5eac7af
matthew-trump <matthew.trump@gmail.com>
2019-04-24 13:28:55 -0700

    merged into immersive simulator service

--- src/app/child.service.ts
+++ src/app/child.service.ts
@@ -1,35 +1,38 @@
 import { Injectable, Inject } from '@angular/core';
-import { Store } from '@ngrx/store';
-import { DOCUMENT } from '@angular/common';
-import { tap } from 'rxjs/operators';
-import { WindowRefService } from './window-ref.service';
+//import { Store } from '@ngrx/store';
+//import { DOCUMENT } from '@angular/common';
+//import { tap } from 'rxjs/operators';
+//import { WindowRefService } from './window-ref.service';
 
-import { ImmersiveSimulatorMessageAction } from './config-state.action';
+//import { ImmersiveSimulatorMessageAction } from './config-state.action';
 
-import { ProjectConfig } from './project-config';
+//import { ProjectConfig } from './project-config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChildService {
 
-  project: ProjectConfig;
+  //project: ProjectConfig;
 
-  constructor(private windowRefService: WindowRefService,
-    private store: Store<any>,
-    @Inject(DOCUMENT) private document: any) {
-
-    this.windowRefService.nativeWindow
-      .addEventListener("message",
-        this.receiveMessage.bind(this), false);
+  constructor(
+    //private windowRefService: WindowRefService,
+    //private store: Store<any>,
+    //@Inject(DOCUMENT) private document: any
+  ) {
+    /** 
+  this.windowRefService.nativeWindow
+    .addEventListener("message",
+      this.receiveMessage.bind(this), false);
+      */
   }
-
+  /** 
   get child() {
     return this.document.getElementById('iframe') ?
       this.document.getElementById('iframe').contentWindow : null;
   }
   receiveMessage(event: MessageEvent) {
-    this.store.dispatch(new ImmersiveSimulatorMessageAction(event));
+    //this.store.dispatch(new ImmersiveSimulatorMessageAction(event));
   }
   sendOutputTtsStatus(requestId: number, status: string) {
     if (!this.child) return;
@@ -44,5 +47,5 @@ export class ChildService {
     console.log("CHILD SERVICE sendMessage", message);
     this.child.postMessage(message, "*");
   }
-
+  */
 }
